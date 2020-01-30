539cda2c6db0fceec22ba2f2aeee7e34c5d3ab43
exirizo <60464978+exirizo@users.noreply.github.com>
2020-01-30 04:11:32 -0500

    Add the bcrypt hasher.

---
+++ src/Bcrypt.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace Rebel\Hashing;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Hash the password using the crypt blowfish algorithm.
+ */
+final class Bcrypt extends AbstractHasher implements Hasher
+{
+    /** @var array $options The bcrypt hasher options. */
+    private array $options = [];
+
+    /**
+     * Construct a new bcrypt hasher.
+     *
+     * @param array $options The bcrypt hasher options.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $options = [])
+    {
+        $this->setOptions($options);
+    }
+
+    /**
+     * Set the bcrypt hasher options.
+     *
+     * @param array $options The bcrypt hasher options.
+     *
+     * @return \Rebel\Hashing\Hasher Returns the hasher.
+     */
+    public function setOptions(array $options = []): Hasher
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $this->options = $resolver->resolve($options);
+        return $this;
+    }
+
+    /**
+     * Comput a new bcrypt hash.
+     *
+     * @param string $password The password to hash.
+     *
+     * @return string Returns the hashed password.
+     */
+    public function compute(string $password): string
+    {
+        return password_hash($password, PASSWORD_BCRYPT, $this->options);
+    }
+
+    /**
+     * Determine if the hash needs a rehash.
+     *
+     * @param string $hash The hash to check.
+     *
+     * @return bool Returns true if the hash needs a rehash and false if not.
+     */
+    public function needsRehash(string $hash): bool
+    {
+        return password_needs_rehash($hash, PASSWORD_BCRYPT, $this->options);
+    }
+
+    /**
+     * Configure the bcrypt hasher options.
+     *
+     * @param OptionsResolver The symfony options resolver.
+     *
+     * @return void Returns nothing.
+     */
+    private function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'cost' => 10,
+        ]);
+        $resolver->setAllowedTypes('cost', 'int');
+    }
+}
