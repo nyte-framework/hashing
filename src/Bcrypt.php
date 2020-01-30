d9c84b904f8db2cc564c3ecf73732d17747867f3
exirizo <60464978+exirizo@users.noreply.github.com>
2020-01-30 06:19:52 -0500

    Cleanup the bcrypt hasher.

--- src/Bcrypt.php
+++ src/Bcrypt.php
@@ -5,11 +5,52 @@ namespace Rebel\Hashing;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Hash the password using the crypt blowfish algorithm.
+ * Hash the password using the bcrypt algorithm.
  */
 final class Bcrypt extends AbstractHasher implements Hasher
 {
-    use BasicHasher;
+    /** @var array $options The bcrypt hasher options. */
+    private array $options = [];
+
+    /**
+     * Construct a new bcrypt hasher.
+     *
+     * @param array $options The bcrypt hasher options.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $options = [])
+    {
+        $this->setOptions($options);
+    }
+
+    /**
+     * Set the bcrypt hasher options.
+     *
+     * @param array $options The bcrypt hasher options.
+     *
+     * @return \Rebel\Hashing\Hasher Returns the hasher.
+     */
+    public function setOptions(array $options = []): Hasher
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $this->options = $resolver->resolve($options);
+        return $this;
+    }
+
+    /**
+     * Verify the password matches the given hash.
+     *
+     * @param string $password The password to check.
+     * @param string $hash     THe hash the password must match.
+     *
+     * @return bool Returns true if the password matches and false if not.
+     */
+    public function verify(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
 
     /**
      * Comput a new bcrypt hash.
