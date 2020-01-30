98380035e37270b9fcfbf9a6171d0fd80dfbccc4
exirizo <60464978+exirizo@users.noreply.github.com>
2020-01-30 06:17:10 -0500

    Add the argon2id hasher.

---
+++ src/Argon2id.php
@@ -0,0 +1,97 @@
+<?php declare(strict_types=1);
+
+namespace Rebel\Hashing;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Hash the password using the argon2id algorithm.
+ */
+final class Argon2id extends AbstractHasher implements Hasher
+{
+    /** @var array $options The argon2i hasher options. */
+    private array $options = [];
+
+    /**
+     * Construct a new argon2id hasher.
+     *
+     * @param array $options The argon2id hasher options.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $options = [])
+    {
+        $this->setOptions($options);
+    }
+
+    /**
+     * Set the argon2id hasher options.
+     *
+     * @param array $options The argon2id hasher options.
+     *
+     * @return \Rebel\Hashing\Hasher Returns the hasher.
+     */
+    public function setOptions(array $options = []): Hasher
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $this->options = $resolver->resolve($options);
+        return $this;
+    }
+
+    /**
+     * Verify the password matches the given hash.
+     *
+     * @param string $password The password to check.
+     * @param string $hash     THe hash the password must match.
+     *
+     * @return bool Returns true if the password matches and false if not.
+     */
+    public function verify(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+
+    /**
+     * Comput a new argon2id hash.
+     *
+     * @param string $password The password to hash.
+     *
+     * @return string Returns the hashed password.
+     */
+    public function compute(string $password): string
+    {
+        return password_hash($password, PASSWORD_ARGON2ID, $this->options);
+    }
+
+    /**
+     * Determine if the hash needs a rehash.
+     *
+     * @param string $hash The hash to check.
+     *
+     * @return bool Returns true if the hash needs a rehash and false if not.
+     */
+    public function needsRehash(string $hash): bool
+    {
+        return password_needs_rehash($hash, PASSWORD_ARGON2ID, $this->options);
+    }
+
+    /**
+     * Configure the argon2id hasher options.
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver The symfony options resolver.
+     *
+     * @return void Returns nothing.
+     */
+    private function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'memory_cost' => PASSWORD_ARGON2_DEFAULT_MEMORY_COST,
+            'time_cost' => PASSWORD_ARGON2_DEFAULT_TIME_COST,
+            'threads' => PASSWORD_ARGON2_DEFAULT_THREADS,
+        ]);
+        $resolver->setAllowedTypes('memory_cost', 'int');
+        $resolver->setAllowedTypes('time_cost', 'int');
+        $resolver->setAllowedTypes('threads', 'int');
+    }
+}
